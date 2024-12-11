b23aa163d5fae2ca51a303deec6cac3242330ce5
Chao Wang <wangchao@msn.cn>
2024-12-11 17:03:00 +0800

    atpos

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,8 @@ let account;
 const okxUniversalProvider = ref(null);
 const universalUi = ref(null);
 
+let okxUniversalConnectUI;
+
 onMounted(async () => {
   universalUi.value = await OKXUniversalConnectUI.init({
     dappMetaData: {
@@ -58,13 +60,65 @@ const handleClick = async () => {
   alert(JSON.stringify(personalSignResult));
 }
 
+
+const connectAptos = async () => {
+  okxUniversalConnectUI = await OKXUniversalConnectUI.init({
+    dappMetaData: {
+      icon: "https://static.okx.com/cdn/assets/imgs/247/58E63FEA47A2B7D7.png",
+      name: "OKX Connect Demo"
+    },
+    actionsConfiguration: {
+      modals: "all"
+    },
+    language: "en_US",
+    uiPreferences: {
+      theme: THEME.LIGHT
+    },
+  });
+
+  var session = await okxUniversalConnectUI.openModal({
+    namespaces: {
+      aptos: {
+        chains: ["aptos:mainnet", // aptos mainnet
+          // "movement:testnet",// movement testnet
+        ],
+      }
+    }
+  })
+
+  console.log("session: ", session);
+}
+
+const sendAptos = async () => {
+  await connectAptos();
+
+  const transactionData = {
+    arguments: [toAddress, `1000}`], // aptos deciaml 10^8
+    function: '0x1::coin::transfer',
+    type: 'entry_function_payload',
+    type_arguments: ['0x1::aptos_coin::AptosCoin'],
+  };
+
+
+  let provider = new OKXAptosProvider(okxUniversalConnectUI)
+
+  console.log("sending transaction...");
+
+  await provider.signAndSubmitTransaction(transactionData, "aptos:mainnet")
+
+}
+
 </script>
 
 <template>
   <div class="h-[100px] flex items-center justify-center">
-    <button @click="handleClick"
+    <!-- <button @click="handleClick"
       class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-3 px-6 rounded-lg shadow-lg transform transition-all duration-300 hover:scale-105 active:scale-95">
       Connect and Sign in
+    </button> -->
+    <button @click="sendAptos"
+      class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-3 px-6 rounded-lg shadow-lg transform transition-all duration-300 hover:scale-105 active:scale-95">
+      Connect Aptos and Transfer to self
     </button>
   </div>
 </template>
